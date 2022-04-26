c8886d5ced7d12f12027136e9f95b9109cd3a6fa
Jessica <85376230+j-pohl@users.noreply.github.com>
2022-04-26 17:45:07 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const slides = document.getElementsByClassName("carousel-item") 
+const slides = document.getElementsByClassName("carousel-item"); 
 let slidePosition = 0; 
-const totalSlides = slides.length 
+const totalSlides = slides.length; 
 
-document.getElementById("carousel-button-prev").addEventListener("click", moveToPrevSlide)
-document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide)
+document.getElementById("carousel-button-prev").addEventListener("click", moveToPrevSlide);
+document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide);
 
 function hideAllSlides() {
-    for (let slide of slides) {
-        slide.classList.remove("carousel-item-visible");
-        slide.classList.add("carousel-item-hidden");
-    }
-};
+  for (let slide of slides) {
+    slide.classList.remove("carousel-item-visible");
+    slide.classList.add("carousel-item-hidden");
+  }
+}
 
 function moveToNextSlide() {
-   hideAllSlides();
-if (slidePosition === totalSlides -1) { 
+  hideAllSlides();
+  if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
-} else {
-    slidePosition ++;
-}
-slides[slidePosition].classList.add("carousel-item-visible")
+  } else {
+    slidePosition++;
+  }
+  slides[slidePosition].classList.add("carousel-item-visible");
 }
 
 function moveToPrevSlide() {
-    console.log("prev slide")
+  console.log("moving to prevous slide");
 }
 
 
