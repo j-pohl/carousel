60f68071255d3f714a7ffa103b5a56f631441a8e
Jessica <85376230+j-pohl@users.noreply.github.com>
2022-04-26 17:22:09 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-const slides = document.getElementsByClassName("carousel-item") // link slides to DOM elements
-
-let slidePosition = 0; // starting index position
-
-const totalSlides = slides.length // total length of array
-
-const moveToNextSlide = () => {
-    
-console.log("moving to next slide")
-if (slidePosition === totalSlides -1) { // if this current slide is the last slide
-    slidePosition = 0 // start from first slide, otherwise error as array only 3 long
-} else {
-    slidePosition ++ // go to next slide
-}
-
-slides[slidePosition].classList.add("carousel-item-visible")
-//add the class name of carousel-item-visible t our current slide
-}
-
-const moveToPrevSlide = () => {
-console.log("moving to prevous slide")
-}
+const slides = document.getElementsByClassName("carousel-item") 
+let slidePosition = 0; 
+const totalSlides = slides.length 
 
 document.getElementById("carousel-button-prev").addEventListener("click", moveToPrevSlide)
 document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide)
 
+const moveToNextSlide = () => {
+    
+console.log("moving to next slide")
+if (slidePosition === totalSlides -1) { 
+    slidePosition = 0 
+} else {
+    slidePosition ++ 
+}
+slides[slidePosition].classList.add("carousel-item-visible")
+}
 
-console.log(slides)
+const moveToPrevSlide = () => {
+    
+}
 
 
-// :)
+
