d77343cbfddc481a89caad63f52cc54523845265
Jessica <85376230+j-pohl@users.noreply.github.com>
2022-04-26 17:40:22 +1000

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,24 @@ const totalSlides = slides.length
 document.getElementById("carousel-button-prev").addEventListener("click", moveToPrevSlide)
 document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide)
 
-const moveToNextSlide = () => {
-    
-console.log("moving to next slide")
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove("carousel-item-visible");
+        slide.classList.add("carousel-item-hidden");
+    }
+};
+
+function moveToNextSlide() {
+   hideAllSlides();
 if (slidePosition === totalSlides -1) { 
-    slidePosition = 0 
+    slidePosition = 0;
 } else {
-    slidePosition ++ 
+    slidePosition ++;
 }
 slides[slidePosition].classList.add("carousel-item-visible")
 }
 
-const moveToPrevSlide = () => {
+function moveToPrevSlide() {
     
 }
 
